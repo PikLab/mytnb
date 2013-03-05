16fc0b1f7f18ddc37b3b36c28bd7192082ab135c
mytnb <Utente@192.168.1.2>
2013-03-05 16:39:12 +0100

    New project mytnb This will become my new Killer App

---
+++ TheNewBoston/src/com/thenewboston/travis/Camera.java
@@ -0,0 +1,84 @@
+package com.thenewboston.travis;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class Camera extends Activity implements View.OnClickListener{
+
+	Button b;
+	ImageButton ib;
+	ImageView iv;
+	
+	Intent i;
+	final static int cameraData = 0;
+	Bitmap bmp;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		// custom
+		setContentView(R.layout.photo);
+		initialize();
+		InputStream is = getResources().openRawResource(R.drawable.triacubes_evo8);
+		bmp = BitmapFactory.decodeStream(is);
+	}
+
+	private void initialize() {
+		// TODO Auto-generated method stub
+		b = (Button) findViewById(R.id.bSetWall);
+		ib = (ImageButton) findViewById(R.id.ibTakePic);
+		iv = (ImageView) findViewById(R.id.ivReturnedPic);
+		
+		b.setOnClickListener(this);
+		ib.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.ibTakePic:
+			i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+			startActivityForResult(i, cameraData);
+			
+			break;
+		case R.id.bSetWall:
+			try {
+				WallpaperManager wpm = WallpaperManager.getInstance(getApplicationContext());
+				wpm.setBitmap(bmp);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		super.onActivityResult(requestCode, resultCode, data);
+		// custom
+		if(resultCode == RESULT_OK){
+			Bundle extras = data.getExtras();
+			bmp = (Bitmap) extras.get("data");
+			iv.setImageBitmap(bmp);
+		}
+	}
+	
+	
+
+}
+
+
+
---
+++ TheNewBoston/src/com/thenewboston/travis/Splash.java
@@ -0,0 +1,38 @@
+package com.thenewboston.travis;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread(){
+			public void run(){
+				try{
+					sleep(1000);
+			} catch (InterruptedException e){
+				e.printStackTrace();
+			} finally{
+				Intent openStartingPoint = new Intent("com.thenewboston.travis.MENU");
+				startActivity(openStartingPoint);
+			}
+		}
+	};
+	timer.start();
+	
+	
+}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+}
